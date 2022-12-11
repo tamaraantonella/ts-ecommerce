28f3d0576b9e7f717c0bc38b80cc2f3a11a464d7
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-11 18:01:24 -0300

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export function Navbar() {
+  return (
+    <nav
+      aria-label="Site Nav"
+      className="mx-auto flex max-w-3xl items-center justify-between p-4 sticky top-0">
+      <Link
+        to="/"
+        className="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100">
+        <span className="sr-only">Logo</span>
+        👋
+      </Link>
+
+      <ul className="flex items-center gap-2 text-sm font-medium text-gray-500">
+        <li className="hidden lg:block">
+          <Link className="rounded-lg px-3 py-2" to="/">
+            {" "}
+            Home{" "}
+          </Link>
+        </li>
+
+        <li>
+          <Link className="rounded-lg px-3 py-2" to="/about">
+            {" "}
+            About{" "}
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            className="inline-flex items-center rounded-lg px-3 py-2"
+            to="/store">
+            Store
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
