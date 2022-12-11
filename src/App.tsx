3ca8e07d15552733c5b17ccb134794d36a793c6e
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-11 18:01:04 -0300

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-
-
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <p>First commit</p>
-    </div>
-  )
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
