db0ccd6eb87fc5a94ec076945949c502824d9bbf
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-12 16:11:21 -0300

    fix Store Item component

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,5 @@
+import { useShoppingCart } from "../context/ShopCartContext";
+
 type StoreItemProps = {
   brand: string;
   category: string;
@@ -24,6 +26,8 @@ export function StoreItem(props: StoreItemProps) {
     title,
     thumbnail,
   } = props;
+  const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeItemFromCart } = useShoppingCart();
+  const quantity = getItemQuantity(id);
   return (
     <div className="bg-white rounded-lg shadow-lg">
       <div className="p-3 h-44">
@@ -34,16 +38,44 @@ export function StoreItem(props: StoreItemProps) {
         <p>Category: {category}</p>
       </div>
       <div className="p-3 w-full">
-        <div><img src={thumbnail} alt={title} className="w-full h-44 object-cover overflow-hidden"/></div>
-        <div className="flex items-center justify-between">
+        <div>
+          <img
+            src={thumbnail}
+            alt={title}
+            className="w-full h-44 object-cover overflow-hidden"
+          />
+        </div>
+        <div className="flex items-center justify-between mt-3">
           <div className="flex-1">
             <p className="text-xl font-bold">${price}</p>
             <p>Rating: {rating}</p>
+            <p>Stock {stock}</p>
           </div>
           <div className=" self-end">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Add to Cart
-            </button>
+            {quantity === 0 ? (
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => increaseCartQuantity(id,stock)}>
+                Add to Cart
+              </button>
+            ) : (
+              <div className=" flex flex-col items-center gap-2 ">
+                <div className="flex items-center justify-center gap-2">
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold  px-2 rounded"
+                    onClick={() => decreaseCartQuantity(id)}>
+                    -
+                  </button>
+                  <div>{quantity}</div>
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold  px-2 rounded"
+                    onClick={() => increaseCartQuantity(id,stock)}>
+                    +
+                  </button>
+                </div>
+                <button onClick={() => removeItemFromCart(id)}>Remove</button>
+              </div>
+            )}
           </div>
         </div>
       </div>
