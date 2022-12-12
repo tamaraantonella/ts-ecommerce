bf0679d690196f50a5c24940b11d2f3315103efb
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-12 19:12:53 -0300

    Add openCart & cartQuantity

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BsCart } from "react-icons/bs";
+import { useShoppingCart } from "../context/ShopCartContext";
+
 export function Cart() {
+  const { openCart, cartQuantity } = useShoppingCart();
   return (
     <div className="cursor-pointer relative w-9">
-      <p className="text-xl">
-
+      <button className="text-xl" onClick={openCart}>
       <BsCart />
-      </p>
+      </button>
       <div className="absolute px-1 bg-red-500 text-white rounded-full text-xs top-0 right-0">
-        0
+        {cartQuantity}
       </div>
     </div>
   );
